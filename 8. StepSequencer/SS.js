fef89420b4c31f8b829454780fee34c102966a57
Ishigami-kun98 <48060336+Ishigami-kun98@users.noreply.github.com>
2023-02-14 19:52:20 +0100

    update

--- 8. StepSequencer/SS.js
+++ 8. StepSequencer/SS.js
@@ -1,6 +1,34 @@
 const X = 8;
 const Y = 8;
 var list = new Array(X);
+var currentRow = -1;
+var currentOutput;
+var midi = null;
+
+if (navigator){
+    navigator.requestMIDIAccess().then(s,f);
+}
+
+function s(s){
+    midi = s;
+    settingUp();
+}
+function f(){
+    console.alert('no midi for you');
+
+}
+
+function settingUp(){
+    var txt = "";
+    midi.outputs.forEach(function(output, key){
+        if (txt){
+            currentOutput = output;
+        }
+        txt += "<option value=" + key + ">" + output.name + "</option>";
+    });
+
+    document.getElementById('p').innerHTML = txt;
+}
 function prepareTable() {
     var stringCells = "";
     
@@ -16,7 +44,7 @@ function prepareTable() {
 function onClick(div){
     var position = div.id.split('_');
     var currentDiv = document.getElementById(div.id);
-    console.log(list[position[0]][position[1]]);
+
     if(list[position[0]][position[1]] == 0){
         currentDiv.style.backgroundColor = "Orange";
         list[position[0]][position[1]] = 1;
@@ -35,4 +63,68 @@ function createArray(){
         list[i] = l;
     }
     console.log(list);
+}
+
+function playNext(forward){
+    if (forward){
+        if( currentRow != -1 ){
+            for(var i = 0; i < X; i++){
+                if (list[i][currentRow] == 1){
+                    document.getElementById(i + "_" + currentRow).style.backgroundColor = "Orange";
+                }else{
+                    document.getElementById(i + "_" + currentRow).style.backgroundColor = "DarkMagenta";
+                }
+            }
+        }
+        currentRow ++;
+        if (currentRow >= 8){
+            currentRow = 0;
+        }
+        for(var i = 0; i < X; i++){
+            
+            if (list[i][currentRow] == 1){
+                document.getElementById(i + "_" + currentRow).style.backgroundColor = "Black";
+                var noteOn = [0x90, 72 - i*2, 60];
+                currentOutput.send(noteOn);
+            }else{
+                document.getElementById(i + "_" + currentRow).style.backgroundColor = "Gold";
+            }
+        }
+    }else{
+        if( currentRow != -1 ){
+            for(var i = 0; i < X; i++){
+                if (list[i][currentRow] == 1){
+                    document.getElementById(i + "_" + currentRow).style.backgroundColor = "Orange";
+                }else{
+                    document.getElementById(i + "_" + currentRow).style.backgroundColor = "DarkMagenta";
+                }
+            }
+        }
+        currentRow --;
+        if (currentRow >= 8){
+            currentRow = 0;
+        }
+        for(var i = 0; i < X; i++){
+            
+            if (list[i][currentRow] == 1){
+                document.getElementById(i + "_" + currentRow).style.backgroundColor = "Black";
+                var noteOn = [0x90, 72 - i*2, 60];
+                currentOutput.send(noteOn);
+            }else{
+                document.getElementById(i + "_" + currentRow).style.backgroundColor = "Gold";
+            }
+        }
+    }
+}
+
+function changeOutput(value){
+    currentOutput = midi.outputs.get(value);
+}
+
+document.onkeydown = checkKey;
+function checkKey(e) {
+    if (e.keyCode == '37') // freccia sx
+       playNext(false);
+    else if (e.keyCode == '39') // freccia dx
+       playNext(true);
 }
